812f4afe8c04c65f50a495ebff577c86da7da9c9
Milton Câmara <miltomcamara@gmail.com>
2019-08-30 00:32:19 -0300

    Commit Inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+declare var $: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'azdonapratica-fronend-app';
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    $(document).ready(() => {
+      $('#elementId').css({ 'background-color': 'yellow', 'font-size': '200%' });
+    });
+  }
 }
